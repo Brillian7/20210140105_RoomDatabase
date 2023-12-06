ac8c9a41b820e2e0f97175777e03715f0301c373
brilly123 <b.amindia.ft21@mail.umy.ac.id>
2023-12-06 13:26:05 +0700

    add form input siswa

--- app/src/main/java/com/example/roomsiswa/ui/halaman/HalamanEntry.kt
+++ app/src/main/java/com/example/roomsiswa/ui/halaman/HalamanEntry.kt
@@ -1,9 +1,22 @@
 package com.example.roomsiswa.ui.halaman
 
+import androidx.compose.material3.ExperimentalMaterial3Api
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
 import com.example.roomsiswa.R
+import com.example.roomsiswa.model.DetailSiswa
 import com.example.roomsiswa.navigasi.DestinasiNavigasi
 
 object DestinasiEntry: DestinasiNavigasi {
     override val route = "item_entry"
     override val titleRes = R.string.entity_siswa
+}
+@OptIn(ExperimentalMaterial3Api::class)
+@Composable
+fun FormInputSiswa(
+    detailSiswa: DetailSiswa,
+    modifier: Modifier = Modifier,
+    onValueChange: (DetailSiswa) -> Unit = {},
+){
+
 }
